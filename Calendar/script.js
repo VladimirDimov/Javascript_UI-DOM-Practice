4b2eaf96ba8ca03c6e941bd036baa29b5bb5dd69
VladimirDimov <vdimov1124@gmail.com>
2015-08-03 10:24:06 +0300

    Calendar rewritten

--- Calendar/script.js
+++ Calendar/script.js
@@ -1,102 +1,99 @@
-function CreateCalendar(selector, events) {
-	var daysOfWeek = ['Mon', 'Tue', 'Wen', 'Thu', 'Fri', 'Sat', 'Sun'];
+function createCalendar(selector, events) {
+    var daysOfWeek = ['Mon', 'Tue', 'Wen', 'Thu', 'Fri', 'Sat', 'Sun'],
+	    allDays = [],
+	    container = document.querySelector(selector),
+        calendarContainer = document.createElement('div');
 
-	var container = document.querySelector(selector);
-	var selectedBox = null;
 
-	var day = (function() {
-		var day = {
-			init: function(date) {
-				this.date = date;
-				this.event = null;
-				return this;
-			},
-			addEvent: function(event) {
-				this.event = event;
-			},
-			render: function() {
-				var month = 'June',
-					year = 2014,
-					date = this.date;
-					var day = daysOfWeek[((date + 5) % 7)];
+    var dayBox = (function () {
+        var dayBox = {
+            init: function (date) {
+                this.date = date;
+                this.dayOfWeek = daysOfWeek[(date + 5) % 7];
+                this.month = 'June';
+                this.events = null;
+                return this;
+            },
+            render: function () {
+                var dayBoxContainer = document.createElement('div');
+                dayBoxContainer.style.display = 'inline-block';
+                dayBoxContainer.style.width = '150px';
+                dayBoxContainer.style.height = '250px';
+                dayBoxContainer.style.position = 'relative';
+                dayBoxContainer.style.border = '1px solid black';
 
-				var dayBox = document.createElement('div');
-				dayBox.className = 'day-box';
-				dayBox.style.display = 'inline-block';
-				dayBox.style.position = 'float-left';
-				dayBox.style.width = '150px';
-				dayBox.style.height = '200px';
-				dayBox.style.border = '1px solid black';
+                var titleContainer = document.createElement('p');
+                dayBoxContainer.appendChild(titleContainer);
+                titleContainer.innerHTML = this.dayOfWeek + ' ' +
+                    this.date + ' ' +
+                    this.month + ' ' +
+                    '2014';
+                titleContainer.style.borderBottom = '1px solid black';
+                titleContainer.style.margin = 'auto';
+                titleContainer.style.textAlign = 'center';
+                titleContainer.style.fontWeight = '900';
+                titleContainer.style.backgroundColor = '#ccc';
+                titleContainer.addEventListener('mouseover', function () {
+                    this.style.backgroundColor = 'gray';
+                });
+                titleContainer.addEventListener('mouseout', function () {
+                    this.style.backgroundColor = '';
+                });
 
-				var titleBox = document.createElement('strong');
-				titleBox.className = 'title-box';
-				dayBox.appendChild(titleBox);
-				titleBox.innerHTML = day + ' ' + date + ' ' + month + ' ' + year;
-				titleBox.style.borderBottom = '1px solid black';
-				titleBox.style.display = 'block';
+                if (this.events !== null) {
+                    var eventContainer = document.createElement('p');
+                    dayBoxContainer.appendChild(eventContainer);
+                    eventContainer.innerHTML = this.events.time + ' ' +
+                        this.events.duration + ' ' +
+                        this.events.title;
+                    eventContainer.style.display = 'inline';
+                    eventContainer.style.position = 'absolute';
+                }
 
-				var eventBox = document.createElement('div');
-				dayBox.appendChild(eventBox);
-				dayBox.style.position = 'relative';			
-				eventBox.style.display = 'table-cell';
+                return dayBoxContainer;
+            }
+        };
 
-				if (this.event !== null) {
-					eventBox.innerHTML = this.event.time + ' ' +
-						this.event.duration + ' ' +
-						this.event.title;
-				};
+        return dayBox;
+    }());
 
-				titleBox.addEventListener('mouseover', function(ev){
-					ev.target.style.backgroundColor = 'red';
-				});
+    for (var date = 1; date <= 30; date++) {
+        allDays.push(Object.create(dayBox).init(date));
+    }
 
-				titleBox.addEventListener('mouseout', function(ev){
-					ev.target.style.backgroundColor = '';
-				});
+    for (var event = 0; event < events.length; event++) {
+        var dayIndex = Number(events[event].date) - 1;
+        allDays[dayIndex].events = events[event];
+    }
 
-				dayBox.addEventListener('click', function(ev){					
-					if (!selectedBox) {
-						selectedBox = this;
-						this.style.backgroundColor = 'gray';
-					};
-					if (selectedBox !== this) {
-						selectedBox.style.backgroundColor = '';
-						this.style.backgroundColor = 'gray';
-						selectedBox = this;
-					};
-				});
+    // render
 
-				return dayBox;
-			}
-		};
+    for (var i = 0; i < allDays.length; i++) {
+        calendarContainer.appendChild(allDays[i].render());
+    }
 
-		return day;
-	})();
+    container.appendChild(calendarContainer);
 
-	var calendar = [];
-
-	for (var i = 1; i <= 30; i++) {
-		var newDay = Object.create(day).init(i);
-
-		calendar.push(newDay);
-	};
-
-	for (var i = 0; i < events.length; i++) {
-		var event = events[i];
-		calendar[event.date - 1].event = event;
-	}
-
-	for (var i = 0; i < calendar.length; i++) {
-		container.appendChild(calendar[i].render());
-	};
-
-	// styles
-	// debugger;
+    debugger;
 }
 
-CreateCalendar('#calendar-container', [{
-	title: 'JavaScript UI & DOM exam',
-	date: 17,
-	time: '10:00',
-	duration: 360
-}]);
+createCalendar('#calendar-container', [
+    {
+        title: 'JavaScript UI & DOM exam',
+        date: 17,
+        time: '10:00',
+        duration: 360
+    },
+    {
+        title: 'JavaScript UI & DOM exam',
+        date: 15,
+        time: '10:00',
+        duration: 360
+    },
+    {
+        title: 'JavaScript UI & DOM exam',
+        date: 3,
+        time: '10:00',
+        duration: 360
+    }
+]);
